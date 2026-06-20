a5fcd91d53887817162296a88a227db78fa5904d
Sivanesan L <sivanesanecetech@gmail.com>
2026-06-20 17:14:57 +0530

    Fix resume download link

--- src/resume.jsx
+++ src/resume.jsx
@@ -7,7 +7,7 @@ function Resume(){
                 <h1>Resume</h1>
                 <p> Download my latest resume to learn more about
                 my education, skills, projects, and experience.</p>
-                <a href="./public/Sivanesan L.pdf" download="Sivanesan_L_Resume.pdf" className="resume-btn">Download Resume</a>
+                <a href="/Sivanesan_L.pdf" download="Sivanesan_L_Resume.pdf" className="resume-btn">Download Resume</a>
             </section>
         </div>
     );
